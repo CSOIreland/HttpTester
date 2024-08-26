921d9bb01e3a0a14bba2b06a8193f94aa5ea6d01
neilfromcork <neilfromcork@gmail.com>
2024-08-26 10:43:13 +0100

    Initial commit

---
+++ HttpTester/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Net;
+
+Console.WriteLine("Please enter the url of the API you wish to access: ");
+string url=Console.ReadLine();
+
+string userAgent;
+
+Console.WriteLine("Please enter a user agent header string, or alternatively just press Enter if you don't want to send a user agent header:");
+userAgent = Console.ReadLine();
+
+using (HttpClient client = new())
+{
+    try
+    {
+        if(!String.IsNullOrEmpty(userAgent))
+            client.DefaultRequestHeaders.Add("User-Agent", userAgent);
+
+        HttpResponseMessage response = await client.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Success:");
+            Console.WriteLine(content);
+        }
+        else
+        {
+            Console.WriteLine("API call failure: "  + response.StatusCode.ToString());
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("System Error:");
+        Console.WriteLine(ex.ToString());
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HttpTester
-Console app to test access to any API. Offers the option of adding a user agent header
+Console app to test access to any API. Offers the option of adding a user agent header.
+
+You will be prompted to enter a user agent header - if you don't want to do this, just press Enter and no user agent header will be sent.
